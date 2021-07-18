427ad275c30ea180475a3feacb49e25379971602
woojooc <cacassag57@gmail.com>
2021-07-18 13:15:26 +0900

    8-2-02

--- src/main/java/study/study/Controller/api/ItemApiController.java
+++ src/main/java/study/study/Controller/api/ItemApiController.java
@@ -1,37 +1,45 @@
 package study.study.Controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import study.study.ifs.CrudInterface;
 import study.study.model.network.Header;
 import study.study.model.network.request.ItemApiRequest;
 import study.study.model.network.response.ItemApiResponse;
+import study.study.service.ItemApiLogicService;
 
 @RestController
 @RequestMapping("/api/item")
 public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
 
+    @Autowired
+    private ItemApiLogicService itemApiLogicService;
 
     @Override
     @PostMapping("")        // /api/item
     public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return null;
+
+        return itemApiLogicService.create(request);
     }
 
     @Override
     @GetMapping("{id}")     // /api/item/id
     public Header<ItemApiResponse> read(@PathVariable Long id) {
-        return null;
+
+        return itemApiLogicService.read(id);
     }
 
     @Override
     @PutMapping("")         // /api/item
     public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-        return null;
+
+        return itemApiLogicService.update(request);
     }
 
     @Override
     @DeleteMapping("{id}")      //  /api/item/id
     public Header delete(@PathVariable Long id) {
-        return null;
+
+        return itemApiLogicService.delete(id);
     }
 }
--- src/main/java/study/study/model/entity/Item.java
+++ src/main/java/study/study/model/entity/Item.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -32,7 +33,7 @@ public class Item {
 
     private String title;
     private String content;
-    private Integer price;
+    private BigDecimal price;
     private String brandName;
 
     private LocalDateTime registeredAt;
