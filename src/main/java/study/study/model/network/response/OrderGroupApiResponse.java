f7aca56465705303cbeadd8dceac74895ad8118c
woojooc <cacassag57@gmail.com>
2021-07-19 09:51:42 +0900

    8-3-02

---
+++ src/main/java/study/study/model/network/response/OrderGroupApiResponse.java
@@ -0,0 +1,41 @@
+package study.study.model.network.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderGroupApiResponse {
+
+    private Long id;
+
+    private String status;
+
+    private String orderType;
+
+    private String revAddress;
+
+    private String revName;
+
+    private String paymentType;
+
+    private BigDecimal totalPrice;
+
+    private Integer totalQuantity;
+
+    private LocalDateTime orderAt;
+
+    private LocalDateTime arrivalDate;
+
+    private Long userId;
+
+    private List<ItemApiResponse> itemApiResponseList;
+}
